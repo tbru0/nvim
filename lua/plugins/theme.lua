01aa13a627fca443226de57c77b9e9febfbaa6ae
TB <tydan2905@gmail.com>
2025-08-24 11:48:06 -0400

    OneDark Theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,15 +1,12 @@
+-- Using Lazy
 return {
-	{
-		"Mofiqul/vscode.nvim",
-		lazy = false, -- Load at startup
-		priority = 1000, -- Make sure it loads before others
-		config = function()
-			require("vscode").setup({
-				-- options go here
-				transparent = false,
-				italic_comments = true,
-			})
-			require("vscode").load()
-		end,
-	},
+    "navarasu/onedark.nvim",
+    priority = 1000,
+    config = function()
+        require("onedark").setup({
+            style = "darker",
+        })
+        -- Enable theme
+        require("onedark").load()
+    end,
 }
